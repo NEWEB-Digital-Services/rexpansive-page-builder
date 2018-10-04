e83645d1c2889c096dacfbe98f1e9086c01fdd81
neweb_agency <info@neweb.info>
2018-10-04 16:24:05 +0200

    activate models list button

--- admin/partials/rexlive-toolbox-fixed.php
+++ admin/partials/rexlive-toolbox-fixed.php
@@ -25,51 +25,52 @@ global $layoutsAvaiable;
     </div>
   </div>
   <div class="tools-container-middle">
-    <div class="middle-tools rexlive-responsive-buttons-wrapper"><?php
-            foreach( $layoutsAvaiable as $index => $layout ) {
-              switch($index){
-                case 0:
-                ?>
-                  <div class="layout-container">
-                    <div class="btn-builder-layout builder-<?php echo $layout['id'] ?>-layout" data-min-width="<?php echo $layout['min']?>" data-max-width="<?php echo $layout['max']; ?>" data-name="<?php echo $layout['id'] ?>">
-                      <?php Rexbuilder_Utilities::get_icon('#A010-Mobile'); ?>
-                    </div>
-                  </div>
-                <?php
-                  break;
-                case 1:
-                ?>
-                  <div class="layout-container">
-                    <div class="btn-builder-layout builder-<?php echo $layout['id'] ?>-layout" data-min-width="<?php echo $layout['min']?>" data-max-width="<?php echo $layout['max']; ?>" data-name="<?php echo $layout['id'] ?>">
-                      <?php Rexbuilder_Utilities::get_icon('#A001-Tablet'); ?>
-                    </div>
-                  </div>
-                <?php
-                  break;
-                case 2:
-                  ?>
-                  <div class="layout-container">
-                    <div class="btn-builder-layout builder-<?php echo $layout['id'] ?>-layout" data-min-width="" data-max-width="" data-name="<?php echo $layout['id'] ?>">
-                      <?php Rexbuilder_Utilities::get_icon('#A011-Desktop'); ?>
-                    </div>
-                  </div>
-                  <?php
-                  break;
-                default:
-                  ?>
-                  <div class="layout-container">
-                    <div class="btn-builder-layout builder-<?php echo $layout['id'] ?>-layout" data-min-width="<?php echo $layout['min']; ?>" data-max-width="<?php echo $layout['max']; ?>" data-name="<?php echo $layout['id'] ?>" data-layout-type="custom">
-                      <?php Rexbuilder_Utilities::get_icon('#A009-Range'); ?>
-                      <div class="layout-custom-number">
-                        <div class="rex-number"><?php echo ($index-2);?></div>
-                      </div>
-                    </div>
-                  </div>
-                  <?php
-                  break;
-              }
-            }
-        ?>
+    <div class="middle-tools rexlive-responsive-buttons-wrapper">
+    <?php
+      foreach( $layoutsAvaiable as $index => $layout ) {
+        switch($index){
+          case 0:
+          ?>
+            <div class="layout-container">
+              <div class="btn-builder-layout builder-<?php echo $layout['id'] ?>-layout" data-min-width="<?php echo $layout['min']?>" data-max-width="<?php echo $layout['max']; ?>" data-name="<?php echo $layout['id'] ?>">
+                <?php Rexbuilder_Utilities::get_icon('#A010-Mobile'); ?>
+              </div>
+            </div>
+          <?php
+            break;
+          case 1:
+          ?>
+            <div class="layout-container">
+              <div class="btn-builder-layout builder-<?php echo $layout['id'] ?>-layout" data-min-width="<?php echo $layout['min']?>" data-max-width="<?php echo $layout['max']; ?>" data-name="<?php echo $layout['id'] ?>">
+                <?php Rexbuilder_Utilities::get_icon('#A001-Tablet'); ?>
+              </div>
+            </div>
+          <?php
+            break;
+          case 2:
+            ?>
+            <div class="layout-container">
+              <div class="btn-builder-layout builder-<?php echo $layout['id'] ?>-layout" data-min-width="" data-max-width="" data-name="<?php echo $layout['id'] ?>">
+                <?php Rexbuilder_Utilities::get_icon('#A011-Desktop'); ?>
+              </div>
+            </div>
+            <?php
+            break;
+          default:
+            ?>
+            <div class="layout-container">
+              <div class="btn-builder-layout builder-<?php echo $layout['id'] ?>-layout" data-min-width="<?php echo $layout['min']; ?>" data-max-width="<?php echo $layout['max']; ?>" data-name="<?php echo $layout['id'] ?>" data-layout-type="custom">
+                <?php Rexbuilder_Utilities::get_icon('#A009-Range'); ?>
+                <div class="layout-custom-number">
+                  <div class="rex-number"><?php echo ($index-2);?></div>
+                </div>
+              </div>
+            </div>
+            <?php
+            break;
+        }
+      }
+      ?>
       <div class="layout-container">
         <div class="builder-config-layouts builder-add-custom-layout">
         <?php Rexbuilder_Utilities::get_icon('#Z001-Plus'); ?>
@@ -80,7 +81,9 @@ global $layoutsAvaiable;
   <div class="tools-container-right">
     <div class="right-tools rexlive-builder-actions">
       <!-- Pulsante per importare i modelli -->
-      <!-- <div class="tool-option"><button class = "btn-models">Import</button></div> -->
+      <div class="tool-option btn-models">
+        <?php Rexbuilder_Utilities::get_icon('#A012-Models-List'); ?>
+      </div>
       <div class="tool-option btn-undo">
         <?php Rexbuilder_Utilities::get_icon('#A003-Undo'); ?>
       </div>
