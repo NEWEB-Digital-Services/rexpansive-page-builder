e22e90418d1e56daa0af5c3c892000c037631de0
neweb_agency <info@neweb.info>
2018-10-23 14:38:45 +0200

    block content position window

--- admin/partials/rexlive-modals-display.php
+++ admin/partials/rexlive-modals-display.php
@@ -776,48 +776,48 @@ defined('ABSPATH') or exit;
     <div id="rex-block-content-position-editor" class="rex-modal rexbuilder-materialize-wrap rex-modal-draggable">
         <?php Rexbuilder_Utilities::close_button(); ?>
         <div class="background-options-area modal-content">            
-            <div id="block-set-class-wrap" class="row valign-wrapper">
-                <div id="block-content-positions-wrapper">
-                    <div class="col">
+            <div id="block-set-class-wrap" class="bl_modal-row">
+                <div id="block-content-positions-wrapper" class="bl_modal__option-wrap bl_jc-c">
+                    <div class="radio-group__wrap">
                         <div>
-                            <input id="rex-bm-content-top-left" type="radio" class="content-position with-gap" name="content-position" value="top-left" />
-                            <label for="rex-bm-content-top-left" class="tooltipped rex-block-position" data-position="bottom" data-tooltip="Top-Left">
+                            <input id="rex-bm-content-top-left" type="radio" class="content-position small-radio with-gap" name="content-position" value="top-left" />
+                            <label for="rex-bm-content-top-left" class="tippy rex-block-position" data-tippy-content="<?php _e('Top-Left Content','rexpansive'); ?>">
                                 <span class="rex-ripple"></span>
                             </label>
-                            <input id="rex-bm-content-top-center" type="radio" class="content-position with-gap" name="content-position" value="top-center" />
-                            <label for="rex-bm-content-top-center" class="tooltipped rex-block-position" data-position="bottom" data-tooltip="Top-Center">
+                            <input id="rex-bm-content-top-center" type="radio" class="content-position small-radio with-gap" name="content-position" value="top-center" />
+                            <label for="rex-bm-content-top-center" class="tippy rex-block-position" data-tippy-content="<?php _e('Top-Center Content','rexpansive'); ?>">
                                 <span class="rex-ripple"></span>
                             </label>
-                            <input id="rex-bm-content-top-right" type="radio" class="content-position with-gap" name="content-position" value="top-right"/>
-                            <label for="rex-bm-content-top-right" class="tooltipped rex-block-position" data-position="bottom" data-tooltip="Top-Right">
+                            <input id="rex-bm-content-top-right" type="radio" class="content-position small-radio with-gap" name="content-position" value="top-right"/>
+                            <label for="rex-bm-content-top-right" class="tippy rex-block-position" data-tippy-content="<?php _e('Top-Right Content','rexpansive'); ?>">
                                 <span class="rex-ripple"></span>
                             </label>
                         </div>
                         <div>
-                            <input id="rex-bm-content-middle-left" type="radio" class="content-position with-gap" name="content-position" value="middle-left" />
-                            <label for="rex-bm-content-middle-left" class="tooltipped rex-block-position" data-position="bottom" data-tooltip="Middle-Left">
+                            <input id="rex-bm-content-middle-left" type="radio" class="content-position small-radio with-gap" name="content-position" value="middle-left" />
+                            <label for="rex-bm-content-middle-left" class="tippy rex-block-position" data-tippy-content="<?php _e('Middle-Left Content','rexpansive'); ?>">
                                 <span class="rex-ripple"></span>
                             </label>
-                            <input id="rex-bm-content-middle-center" type="radio" class="content-position with-gap" name="content-position" value="middle-center" />
-                            <label for="rex-bm-content-middle-center" class="tooltipped rex-block-position" data-position="bottom" data-tooltip="Middle-Center">
+                            <input id="rex-bm-content-middle-center" type="radio" class="content-position small-radio with-gap" name="content-position" value="middle-center" />
+                            <label for="rex-bm-content-middle-center" class="tippy rex-block-position" data-tippy-content="<?php _e('Middle-Center Content','rexpansive'); ?>">
                                 <span class="rex-ripple"></span>
                             </label>
-                            <input id="rex-bm-content-middle-right" type="radio" class="content-position with-gap" name="content-position" value="middle-right" />
-                            <label for="rex-bm-content-middle-right" class="tooltipped rex-block-position" data-position="bottom" data-tooltip="Middle-Right">
+                            <input id="rex-bm-content-middle-right" type="radio" class="content-position small-radio with-gap" name="content-position" value="middle-right" />
+                            <label for="rex-bm-content-middle-right" class="tippy rex-block-position" data-tippy-content="<?php _e('Middle-Right Content','rexpansive'); ?>">
                                 <span class="rex-ripple"></span>
                             </label>
                         </div>
                         <div>
-                            <input id="rex-bm-content-bottom-left" type="radio" class="content-position with-gap" name="content-position" value="bottom-left" />
-                            <label for="rex-bm-content-bottom-left" class="tooltipped rex-block-position" data-position="bottom" data-tooltip="Bottom-Left">
+                            <input id="rex-bm-content-bottom-left" type="radio" class="content-position small-radio with-gap" name="content-position" value="bottom-left" />
+                            <label for="rex-bm-content-bottom-left" class="tippy rex-block-position" data-tippy-content="<?php _e('Bottom-Left Content','rexpansive'); ?>">
                                 <span class="rex-ripple"></span>
                             </label>
-                            <input id="rex-bm-content-bottom-center" type="radio" class="content-position with-gap" name="content-position" value="bottom-center" />
-                            <label for="rex-bm-content-bottom-center" class="tooltipped rex-block-position" data-position="bottom" data-tooltip="Bottom-Center">
+                            <input id="rex-bm-content-bottom-center" type="radio" class="content-position small-radio with-gap" name="content-position" value="bottom-center" />
+                            <label for="rex-bm-content-bottom-center" class="tippy rex-block-position" data-tippy-content="<?php _e('Bottom-Center Content','rexpansive'); ?>">
                                 <span class="rex-ripple"></span>
                             </label>
-                            <input id="rex-bm-content-bottom-right" type="radio" class="content-position with-gap" name="content-position" value="bottom-right" />
-                            <label for="rex-bm-content-bottom-right" class="tooltipped rex-block-position" data-position="bottom" data-tooltip="Bottom-Right">
+                            <input id="rex-bm-content-bottom-right" type="radio" class="content-position small-radio with-gap" name="content-position" value="bottom-right" />
+                            <label for="rex-bm-content-bottom-right" class="tippy rex-block-position" data-tippy-content="<?php _e('Bottom-Right Content','rexpansive'); ?>">
                                 <span class="rex-ripple"></span>
                             </label>
                         </div>
