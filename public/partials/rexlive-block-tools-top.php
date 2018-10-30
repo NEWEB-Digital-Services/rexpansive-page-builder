ad2c65fa75b0144df4ad4c8dd7f58668b82eda21
neweb_agency <info@neweb.info>
2018-10-30 09:06:12 +0100

    update: styling insert link on text editor

--- public/partials/rexlive-block-tools-top.php
+++ public/partials/rexlive-block-tools-top.php
@@ -17,14 +17,14 @@ defined( 'ABSPATH' ) or exit;
             <div class="el-size-viewer tool-indicator"></div>
         </div>
 
-        <div class="bl_d-iflex bl_ai-c block-toolBox__editor-tools">
+        <div class="bl_d-iflex bl_ai-c block-toolBox__editor-tools tippy" data-tippy-content="<?php _e('Text','rexpansive'); ?>">
             <div class="tool-button tool-button--inline edit-block-content<?php echo ( "" !== trim( $content ) ? ' tool-button--hide' : '' ); ?>">
                 <?php Rexbuilder_Utilities::get_icon('#B003-Text'); ?>
             </div>
-            <div class="tool-button tool-button--inline builder-edit-slider<?php echo ( !$block_has_slider ? ' tool-button--hide' : '' ); ?>">
+            <div class="tool-button tool-button--inline builder-edit-slider<?php echo ( !$block_has_slider ? ' tool-button--hide' : '' ); ?> tippy" data-tippy-content="<?php _e('RexSlider','rexpansive'); ?>">
                 <?php Rexbuilder_Utilities::get_icon('#Z003-RexSlider'); ?>
             </div><!-- // rexslider -->
-            <div class="tool-button tool-button--inline edit-block-content-position<?php echo ( "" === trim( $content ) ? ' tool-button--hide' : '' ); ?>">
+            <div class="tool-button tool-button--inline edit-block-content-position<?php echo ( "" === trim( $content ) ? ' tool-button--hide' : '' ); ?> tippy" data-tippy-content="<?php _e('Content Position','rexpansive'); ?>">
                 <?php Rexbuilder_Utilities::get_icon('#C005-Layout'); ?>
             </div>
         </div>
@@ -42,29 +42,29 @@ defined( 'ABSPATH' ) or exit;
                 
 				<div class="tool-button_list">
                     <div class="tool-button--double-icon--wrap tool-button_list--item<?php echo ( !$not_has_image ? ' tool-button--hide' : '' ); ?>">
-                        <div class="tool-button tool-button--inline edit-block-image tippy">
+                        <div class="tool-button tool-button--inline edit-block-image tippy" data-tippy-content="<?php _e('Background Image','rexpansive'); ?>">
                             <?php Rexbuilder_Utilities::get_icon('#Z002-Image-Full'); ?>
                         </div>                        
                     </div><!-- // Change Block image background -->
 
-                    <div class="tool-button--double-icon--wrap tool-button_list--item">
+                    <div class="tool-button--double-icon--wrap tool-button_list--item tippy" data-tippy-content="<?php _e('Background Color','rexpansive'); ?>">
                         <input class="spectrum-input-element" type="text" name="edit-block-color-background" value="">
                     </div><!-- // Change Block color background -->
 
-                    <div class="tool-button--double-icon--wrap tool-button--opacity-preview tool-button_list--item<?php echo ( !$not_has_overlay ? ' tool-button--hide' : '' ); ?>">
+                    <div class="tool-button--double-icon--wrap tool-button--opacity-preview tool-button_list--item<?php echo ( !$not_has_overlay ? ' tool-button--hide' : '' ); ?> tippy" data-tippy-content="<?php _e('Overlay','rexpansive'); ?>">
                         <input class="spectrum-input-element" type="text" name="edit-block-overlay-color" value="<?php echo esc_attr( $atts['overlay_block_color'] ); ?>">
                         <div class="tool-button tool-button--inline tool-button--empty tool-button--color-preview-texture"></div>
                         
                     </div><!-- // Change Block overlay color -->
 
-                    <div class="tool-button--double-icon--wrap tool-button_list--item<?php echo ( !$not_has_video ? ' tool-button--hide' : '' ); ?>">
+                    <div class="tool-button--double-icon--wrap tool-button_list--item<?php echo ( !$not_has_video ? ' tool-button--hide' : '' ); ?> tippy" data-tippy-content="<?php _e('Background Video','rexpansive'); ?>">
                         <div class="tool-button tool-button--inline edit-block-video-background">
                             <?php Rexbuilder_Utilities::get_icon('#Z006-Video'); ?>
                         </div>
                     </div><!-- // Change Block Video background -->
 
 
-                    <div class="tool-button tool-button--inline tool-button_list--item builder-edit-slider">
+                    <div class="tool-button tool-button--inline tool-button_list--item builder-edit-slider tippy" data-tippy-content="<?php _e('RexSlider','rexpansive'); ?>">
                         <?php Rexbuilder_Utilities::get_icon('#Z003-RexSlider'); ?>
                     </div><!-- // rexslider -->
                 </div>
