d6feb6eaace4e1c3128604973ef0407ada426791
Simone Forgiarini <info@neweb.info>
2019-06-21 17:29:06 +0200

    update: border animation

--- public/js/build/parallax-section.js
+++ public/js/build/parallax-section.js
@@ -15,7 +15,8 @@
     this.element = null;
     this.goingUp = false;
     this.goingDown = false;
-    this.scrollLastPosition = null; 
+    this.scrollLastPosition = null;
+    this.borderAnimationEl = {};
 
     // get element as first argument
     if (arguments[0]) {
@@ -23,7 +24,8 @@
     }
 
     var defaults = {
-      offset: 0
+      offset: 0,
+      borderAnimation: false
     };
 
     // Create options by extending defaults with the passed in arugments
@@ -37,6 +39,26 @@
     this.scrollLastPosition = scrollDocumentPositionTop();
     // this.element.style.maxHeight = '100vh';
 
+    // prepare animations if prsents
+    if ( this.options.borderAnimation ) {
+      this.borderAnimationEl.el = document.createElement('div');
+      addClass( this.borderAnimationEl.el, 'parallax__border-animation__wrap' );
+      this.borderAnimationEl.bt = document.createElement('div');
+      addClass( this.borderAnimationEl.bt, 'parallax__border-animation__top' );
+      this.borderAnimationEl.el.appendChild( this.borderAnimationEl.bt );
+      this.borderAnimationEl.br = document.createElement('div');
+      addClass( this.borderAnimationEl.br, 'parallax__border-animation__right' );
+      this.borderAnimationEl.el.appendChild( this.borderAnimationEl.br );
+      this.borderAnimationEl.bb = document.createElement('div');
+      addClass( this.borderAnimationEl.bb, 'parallax__border-animation__bottom' );
+      this.borderAnimationEl.el.appendChild( this.borderAnimationEl.bb );
+      this.borderAnimationEl.bl = document.createElement('div');
+      addClass( this.borderAnimationEl.bl, 'parallax__border-animation__left' );
+      this.borderAnimationEl.el.appendChild( this.borderAnimationEl.bl );
+
+      this.element.appendChild( this.borderAnimationEl.el );
+    }
+
     // first load check
     handleParallax.call(this);
     window.addEventListener('scroll', handleParallax.bind(this));
@@ -44,7 +66,7 @@
 
   // private shared vars
   var windowInnerHeight = document.documentElement.clientHeight;
-  window.addEventListener('resize', updateWindowInnerHeight);
+  window.addEventListener('resize', updateWindowInnerHeight); 
 
   /**
    * Handling the scrolling of the viewport and the parallax logic
@@ -73,7 +95,8 @@
 
     // element scroll information
     var elScrollTop = offsetTop(this.element, windowScrollTop);
-    var elScrollBottom = elScrollTop + this.element.offsetHeight;
+    var elHeight = this.element.offsetHeight;
+    var elScrollBottom = elScrollTop + elHeight;
 
     // eventually offset
     var windowOffset = windowInnerHeight * this.options.offset;
@@ -92,11 +115,15 @@
       removeClass(this.element, 'visibile');
     }
 
+    // initial classes based on element position
+    // relative to the viewport
     if ( !this.goingDown && !this.goingUp ) {
       if ( windowScrollTop < elScrollTop ) {
         addClass(this.element, 'vtest-top');
       } else if ( windowScrollBottom > elScrollBottom ) {
         addClass(this.element, 'vtest-bottom');
+      } else if ( windowScrollTop > elScrollTop && windowScrollBottom < elScrollBottom ) {
+        addClass(this.element, 'sticky');
       }
     }
 
@@ -112,7 +139,7 @@
           addClass(this.element, 'vtest-top');
         }
       }
-    } 
+    }
     // else if the bottom element reaches the bottom view
     else if ( elScrollBottom >= (windowScrollBottom - step) && elScrollBottom <= (windowScrollBottom + step) )
     {
@@ -127,7 +154,48 @@
         addClass(this.element, 'sticky');
       }
     }
+
+    if ( this.options.borderAnimation ) {
+      var spt = windowScrollTop+( windowInnerHeight / 2 );
+      if ( hasClass( this.element, 'sticky' ) ) {
+        var v = 1 - ( spt - elScrollTop ) / elHeight;
+        scaleBorder.call(this, v );
+        // console.log('A[' + elScrollTop + ',' + 90 + ']', 'B[' + elScrollBottom + ',' + 0 + ']');
+        // if ( this.goingDown ) {
+        //   var easeV = linearValue( elScrollTop, elScrollBottom, 90, 0, windowScrollBottom );
+        //   var easeVmap = easeV / 90;
+        //   scaleBorder.call(this, easeVmap );
+        // } else if (this.goingUp ) {
+        //   var easeV = linearValue( elScrollTop, elScrollBottom, 90, 0, windowScrollTop );
+        //   var easeVmap = easeV / 90;
+        //   scaleBorder.call(this, easeVmap );
+        // }        
+      } else {
+        if ( spt < elScrollTop ) {
+          scaleBorder.call(this, 1 );
+        } else if ( spt > elScrollBottom ) {
+          scaleBorder.call(this, 0 );
+        }
+      }
+    }
   };
+
+  function scaleBorder( val ) {
+    this.borderAnimationEl.bt.style.transform = 'scale(1,' + val + ')';
+    this.borderAnimationEl.bb.style.transform = 'scale(1,' + val + ')';
+    this.borderAnimationEl.br.style.transform = 'scale(' + val + ',1)';
+    this.borderAnimationEl.bl.style.transform = 'scale(' + val + ',1)';
+  }
+
+  function linearValue( x1, x2, y1, y2, x ) {
+    var m = ( y1 - y2 ) / ( x1 - x2 );
+    var q = ( x1*y2 - x2*y1 ) / ( x1 - x2 );
+    return m * x + q;
+  }
+
+  function mapVal( min, max, a, b, x ) {
+    return ( ( ( b - a ) * ( x - min ) ) / ( max - min ) ) + a;
+  }
 
   /**
    * Updating the window inner height only if occurs a window resize
@@ -173,6 +241,49 @@
     scrollTop = 'undefined' !== typeof scrollTop ? scrollTop : ( window.pageYOffset || document.documentElement.scrollTop );
     var rect = el.getBoundingClientRect();
     return rect.top + scrollTop;
+  }
+
+  // bezier testing
+  var beizerPoints = [
+    { x: 0, y: 0 },
+    { x: 0.250, y: 0.100 }, 
+    { x: 0.250, y: 1 }, 
+    { x: 1, y: 1 }
+  ];
+  var coefficients = {
+    cx: 0,
+    bx: 0,
+    ax: 0,
+    cy: 0,
+    by: 0,
+    ay: 0
+  };
+
+  calcCoefficients(beizerPoints);
+  // for ( var i = 0; i <= 1; i = i+0.01 )  {
+  //   console.log(applyBeizier(i));
+  // }
+
+  function calcCoefficients( points ) {
+    coefficients.cx = 3.0 * (points[1].x - points[0].x);
+    coefficients.bx = 3.0 * (points[2].x - points[1].x) - coefficients.cx;
+    coefficients.ax = points[3].x - points[0].x - coefficients.cx -coefficients. bx;
+    
+    coefficients.cy = 3.0 * (points[1].y - points[0].y);
+    coefficients.by = 3.0 * (points[2].y - points[1].y) - coefficients.cy;
+    coefficients.ay = points[3].y - points[0].y - coefficients.cy - coefficients.by;
+  }
+
+  function applyBeizier( t, points ) {
+    points = 'undefined' !== typeof points ? points : [{ x: 0, y: 0 }];
+    var result = {};
+    tSquared = t * t;
+    tCubed = tSquared * t;
+    
+    result.x = (coefficients.ax * tCubed) + (coefficients.bx * tSquared) + (coefficients.cx * t) + points[0].x;
+    result.y = (coefficients.ay * tCubed) + (coefficients.by * tSquared) + (coefficients.cy * t) + points[0].y;
+    
+    return result;
   }
 
   // Utility method to extend defaults with user options
