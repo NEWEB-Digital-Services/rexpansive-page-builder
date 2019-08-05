62b7cad2850470dd52cd4bcd3fccb4228ab2c3bc
robtonino <robertotonino@hotmail.it>
2019-08-05 22:33:11 +0200

    added partial media library opener for the models in the list

--- admin/js/builderlive/Rexlive_Lateral_Menu.js
+++ admin/js/builderlive/Rexlive_Lateral_Menu.js
@@ -5,6 +5,8 @@
 var Model_Lateral_Menu = (function ($) {
   "use strict";
   var rexmodel_lateral_menu;
+  var image_uploader_frame_direct;  //used for the media library opener
+
   var _linkDocumentListeners = function () {
     rexmodel_lateral_menu.$close_button.click(function (e) {
       e.preventDefault();
@@ -65,113 +67,167 @@ var Model_Lateral_Menu = (function ($) {
     });
 
     /**
-     * Write on console "ciao Roberto"
+     * Write on console "ciao Roberto" and opens media library
      * @param  {null}
      * @return {null}
      */
     Rexlive_Base_Settings.$document.on('click', '.model__element--edit', function (e) {
+      //test
       Model_Import_Modal.writeOnConsole();
-    });
 
-    /**
-     * Edit a model background image
-     * @since 2.0.0
-     */
-    Rexlive_Base_Settings.$document.on('click', '.edit-model-image', function(e) {
-      var $btn = $(e.target);
-      var $elem = $btn.parents(".grid-stack-item");
-      var $section = $elem.parents(".rexpansive_section");
-      var rex_block_id = $elem.attr("data-rexbuilder-block-id");
-      var sectionID = $section.attr("data-rexlive-section-id");
-      var modelNumber =
-        typeof $section.attr("data-rexlive-model-number") != "undefined"
-          ? $section.attr("data-rexlive-model-number")
-          : "";
-      var $elemData = $elem.children(".rexbuilder-block-data");
-      var $itemContent = $elem.find(".grid-item-content");
-
-      var tools = '';
-      var $btn_container = $btn.parents('.rexlive-block-toolbox');
-      if( $btn_container.hasClass('bottom-tools') ) {
-        tools = 'bottom';
-      } else if ($btn_container.hasClass('top-tools')) {
-        tools = 'top';
+      // If the frame is already opened, return it
+      if (image_uploader_frame_direct) {
+        image_uploader_frame_direct
+          .state("live-image")
+          .set("liveTarget", ""/*info.sectionTarget*/)
+          .set("selected_image", ""/*info.idImage*/)
+          .set("eventName", ""/*info.returnEventName*/)
+          .set("data_to_send", ""/*info.data_to_send*/)
+        image_uploader_frame_direct.open();
+        return;
       }
 
-      var idImage =
-        typeof $elemData.attr("data-id_image_bg_block") == "undefined"
-          ? ""
-          : $elemData.attr("data-id_image_bg_block");
-      var imageUrl =
-        typeof $elemData.attr("data-image_bg_block") == "undefined"
-          ? ""
-          : $elemData.attr("data-image_bg_block");
-      var width =
-        typeof $itemContent.attr("data-background_image_width") == "undefined"
-          ? ""
-          : $itemContent.attr("data-background_image_width");
-      var height =
-        typeof $itemContent.attr("data-background_image_height") == "undefined"
-          ? ""
-          : $itemContent.attr("data-background_image_height");
-      var activeImage =
-        typeof $elemData.attr("data-image_bg_elem_active") != "undefined"
-          ? $elemData.attr("data-image_bg_elem_active")
-          : true;
-      var defaultTypeImage =
-        $elem.parents(".grid-stack-row").attr("data-layout") == "fixed"
-          ? "full"
-          : "natural";
-      var typeBGimage =
-        ( typeof $elemData.attr("data-type_bg_block") == "undefined" || "" == $elemData.attr("data-type_bg_block") )
-          ? defaultTypeImage
-          : $elemData.attr("data-type_bg_block");
-      var activePhotoswipe =
-        typeof $elemData.attr("data-photoswipe") == "undefined"
-          ? ""
-          : $elemData.attr("data-photoswipe");
-      var imageSize = typeof $elemData.attr("data-image_size") == "undefined" ? "" : $elemData.attr("data-image_size");
-
-      var activeImage = true;
-
-      var data = {
-        eventName: "rexlive:openLiveImageUploader",
-        live_uploader_data: {
-          idImage: activeImage ? idImage : "",
-          urlImage: activeImage ? imageUrl : "",
-          width: activeImage ? width : "",
-          height: activeImage ? height : "",
-          typeBGimage: activeImage ? typeBGimage : "",
-          photoswipe: activeImage ? activePhotoswipe : "",
-          imageSize: activeImage ? imageSize :"",
-          active: activeImage,
-          sectionTarget: {
-            sectionID: sectionID,
-            modelNumber: modelNumber,
-            rexID: rex_block_id
+      //create a new Library, base on defaults
+      //you can put your attributes in
+      var insertImage = wp.media.controller.Library.extend({
+        defaults: _.defaults(
+          {
+            id: "live-image",
+            title: "Edit Image",
+            allowLocalEdits: true,
+            displaySettings: true,
+            displayUserSettings: true,
+            multiple: false,
+            library: wp.media.query({ type: "image" }),
+            liveTarget: ""/*info.sectionTarget*/,
+            eventName: ""/*info.returnEventName*/,
+            data_to_send: ""/*info.data_to_send*/,
+            selected_image: ""/*info.idImage*/,
+            type: "image" //audio, video, application/pdf, ... etc
           },
-          // returnEventName: "rexlive:apply_background_image_block",
-          // data_to_send: {
-          //   idImage: activeImage ? idImage : "",
-          //   urlImage: activeImage ? imageUrl : "",
-          //   width: activeImage ? width : "",
-          //   height: activeImage ? height : "",
-          //   typeBGimage: activeImage ? typeBGimage : "",
-          //   photoswipe: activeImage ? activePhotoswipe : "",
-          //   imageSize: activeImage ? imageSize :"",
-          //   active: activeImage,
-          //   updateBlockHeight: true,
-          //   tools: tools,        
-          //   sectionTarget: {
-          //     sectionID: sectionID,
-          //     modelNumber: modelNumber,
-          //     rexID: rex_block_id
-          //   },
-          // }
-        },
-      };
+          wp.media.controller.Library.prototype.defaults
+        )
+      }); 
 
-      Rexbuilder_Util_Editor.sendParentIframeMessage(data);
+      //Setup media frame
+      image_uploader_frame_direct = wp.media({
+        button: { text: "Select" },
+        state: "live-image",
+        states: [new insertImage()]
+      });
+
+      // prevent attachment size strange selections
+      // image_uploader_frame_direct.on('selection:toggle', function(e) {
+      //   var attachmentSizeEl = document.querySelector( 'select[name="size"]' );
+      //   if ( attachmentSizeEl ) {
+      //     attachmentSizeEl.value = 'full';
+      //   }
+      // });
+
+      //reset selection in popup, when open the popup
+      // image_uploader_frame_direct.on("open", function() {
+      //   var attachment;
+      //   var selection = image_uploader_frame_direct
+      //     .state("live-image")
+      //     .get("selection");
+
+      //   //remove all the selection first
+      //   selection.each(function(video) {
+      //     attachment = wp.media.attachment(video.attributes.id);
+      //     attachment.fetch();
+      //     selection.remove(attachment ? [attachment] : []);
+      //   });
+
+      //   var image_id = image_uploader_frame_direct
+      //     .state("live-image")
+      //     .get("selected_image");
+
+      //   var image_info = image_uploader_frame_direct
+      //     .state("live-image")
+      //     .get("data_to_send");
+
+      //   // Check the already inserted image
+      //   if (image_id) {
+      //     attachment = wp.media.attachment(image_id);
+      //     attachment.fetch();
+
+      //     selection.add(attachment ? [attachment] : [], { size: 'thumbnail' });
+      //   }
+      // });
+
+      image_uploader_frame_direct.on("select", function() {
+        var state = image_uploader_frame_direct.state("live-image");
+        var sectionTarget = state.get("liveTarget");
+        var eventName = state.get("eventName");
+        var data_to_send = state.get("data_to_send");
+
+        var selection = state.get("selection");
+        var data = {
+          eventName: eventName,
+          data_to_send: {
+            // info: info,
+            // media: [],
+            sectionTarget: sectionTarget,
+            target: sectionTarget
+          }
+          // data_to_send: data_to_send
+        };
+
+        if (!selection) return;
+
+        //to get right side attachment UI info, such as: size and alignments
+        //org code from /wp-includes/js/media-editor.js, arround `line 603 -- send: { ... attachment: function( props, attachment ) { ... `
+        selection.each(function(attachment) {
+          var display = state.display(attachment).toJSON();
+          var obj_attachment = attachment.toJSON();
+
+          // If captions are disabled, clear the caption.
+          if (!wp.media.view.settings.captions) delete obj_attachment.caption;
+
+          display = wp.media.string.props(display, obj_attachment);
+
+          // var to_send = {
+          //   media_info: obj_attachment,
+          //   display_info: display
+          // };
+
+          // data.data_to_send.media.push(to_send);
+          data.data_to_send.idImage = obj_attachment.id;
+          data.data_to_send.urlImage = display.src;
+          data.data_to_send.width = display.width;
+          data.data_to_send.height = display.height;
+
+          if( 'undefined' !== typeof data_to_send.photoswipe ) {
+            data.data_to_send.photoswipe = data_to_send.photoswipe;
+          }
+
+          if( 'undefined' !== typeof data_to_send.active ) {
+            data.data_to_send.active = data_to_send.active;
+          }
+
+          if( 'undefined' !== typeof data_to_send.typeBGimage ) {
+            data.data_to_send.typeBGimage = data_to_send.typeBGimage;
+          }
+        });
+
+        // Synch top toolbar tools
+        Rexbuilder_Util_Admin_Editor.highlightRowSetData({
+          image_bg_section_active: data.data_to_send.active,
+          image_bg_section: data.data_to_send.urlImage,
+          id_image_bg_section: data.data_to_send.idImage
+        });
+        Rexbuilder_Util_Admin_Editor.updateBkgrImgTool();
+
+        data.data_to_send.tools = data_to_send.tools;
+
+        // Launch image insert event to the iframe
+        Rexbuilder_Util_Admin_Editor.sendIframeBuilderMessage(data);
+      });
+
+      image_uploader_frame_direct.on("close", function() {});
+
+      //now open the popup
+      image_uploader_frame_direct.open();
     });
   } 
 
@@ -188,7 +244,7 @@ var Model_Lateral_Menu = (function ($) {
 
     _linkDocumentListeners();
   };
-  
+
   return {
     init: _init,
     openModal: _openModal,
