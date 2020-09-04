8f4e570070db38542490fa4749bdcafaecd231fc
Roberto Tonino <roberto.tonino5@gmail.com>
2020-09-04 15:40:47 +0200

    updated: changed version to 2.0.9

--- gulpfile.js
+++ gulpfile.js
@@ -468,7 +468,7 @@ exports.build = series(
 );
 
 /* ---- BUILD LIVE PLUGIN VERSION ----- */
-var live_zip_name = 'Premium-208-Rexpansive-Builder.zip';
+var live_zip_name = 'Premium-209-Rexpansive-Builder.zip';
 var live_folder_name = 'rexpansive-page-builder';
 
 var live_file_map = [
