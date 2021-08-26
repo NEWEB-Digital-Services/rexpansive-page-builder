d2905047d7f14f4bf3a8a2a58c4cb5d795efd523
Stefano Panipucci <stefano.panipucci@gmail.com>
2021-08-26 12:21:17 +0200

    feat: update script to change version on main plugin file and on zip filename

--- scripts/start-new-version.js
+++ scripts/start-new-version.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const fs = require('fs')
 const readline = require('readline')
+const { version } = require('typescript')
 
 // const rl = readline.createInterface({
 // 	input: process.stdin,
@@ -25,5 +26,35 @@ const packageJson = require(packageJsonFilePath)
 
 packageJson.version = newVersion
 fs.writeFileSync(packageJsonFilePath, JSON.stringify(packageJson, null, '\t') + '\n')
+
+const mainPluginFilePath = 'rexpansive-builder.php';
+
+fs.readFile(mainPluginFilePath, 'utf8', (err, data) => {
+	if (err) throw err;
+
+	const newCommentString = `* Version:           ${newVersion}`
+	const newConstantString = `define( 'REXPANSIVE_BUILDER_VERSION', '${newVersion}' );`;
+	let result = data
+	result = result.replace(/\* Version:\s*[\w.,']+/, newCommentString)
+	result = result.replace(/define\(\s*\'REXPANSIVE_BUILDER_VERSION\'\,\s*[\w.,']+\s*\)\;/, newConstantString);
+
+	fs.writeFile(mainPluginFilePath, result, 'utf8', function (err) {
+		if (err) return console.log(err);
+	});
+});
+
+const gulpFile = 'gulpfile.js'
+fs.readFile(gulpFile, 'utf8', (err, data) => {
+	if (err) throw err;
+
+	const cleanedVersion = newVersion.replace(/\./g, '')
+	const newZipVersionString = `Premium-${cleanedVersion}-Rexpansive-Builder.zip`
+	let result = data
+	result = result.replace(/Premium-\d+-Rexpansive-Builder.zip/, newZipVersionString)
+
+	fs.writeFile(gulpFile, result, 'utf8', function (err) {
+		if (err) return console.log(err);
+	});
+});
 
 // rl.close();
