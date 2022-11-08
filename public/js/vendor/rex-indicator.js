24ae10b99bf1da45955c760b7c84a9f9117a869e
Stefano Panipucci <stefano.panipucci@gmail.com>
2022-11-08 14:42:38 +0100

    wip(@rex-indicator): set indicator dimensions and move it on the DOM

--- public/js/vendor/rex-indicator.js
+++ public/js/vendor/rex-indicator.js
@@ -8,6 +8,18 @@
 } )( 'undefined' !== typeof window ? window : this, function() {
 	const instances = []
 
+	/**
+	 * Handling indicator positions on resize
+	 * Keeping a single resize handler with multiple callbacks 
+	 */
+	const resizeCallbacks = []
+		function globalResizeHandler() {
+		for (let i = 0; i < resizeCallbacks.length; i++) {
+			if (null === resizeCallbacks[i]) continue
+			resizeCallbacks[i].call()
+		}
+	}
+
 	function RexIndicator() {
 		this.element = null;
 		this.indicator = null
@@ -30,6 +42,7 @@
 			this.options = defaults;
 		}
 
+		// get eventually inline options
 		var inlineOptionsAttr = this.element.getAttribute('data-rex-indicator-options')
 		if (null !== inlineOptionsAttr) {
 			try {
@@ -41,11 +54,68 @@
 		}
 
 		this.indicator = this.element.querySelector(this.options.indicator)
-		this.block_ref = parents(this.element, this.options.block_parent)
-		this.after_ref = parents(this.element, this.options.after)
-		console.log(this)
+		this.block_ref = parents(this.element, this.options.block_parent).pop()
+		this.after_ref = parents(this.element, this.options.after).pop()
+		this.indicator_moved_parent = null
+
+		init.call(this)
+
 		instances.push( this );
+
+		console.log(this)
 	}
+
+	function init() {
+		move_indicator.call(this)
+		set_indicator_dimension.call(this)
+	}
+
+	function move_indicator() {
+		this.after_ref.after(this.indicator)
+		this.indicator_moved_parent = this.indicator.parentElement
+	}
+
+    /**
+     * Set indicator dimension
+     * @returns void
+     */
+    function set_indicator_dimension() {
+      if (this.options.to_amount === 'auto') return
+
+      // todo: refactor
+      switch (this.options.to) {
+        case 'left': {
+          const L = this.element.getBoundingClientRect().x
+          const l = this.indicator_moved_parent.getBoundingClientRect().x
+          this.indicator.style.setProperty('--rex-indicator-wrap-width', `${L - l}px`)
+          break
+        }
+        case 'right': {
+          const info = this.indicator_moved_parent.getBoundingClientRect()
+          const L = this.element.getBoundingClientRect().x
+          const newWidth = info.x + info.width - L
+          this.indicator.style.setProperty('--rex-indicator-wrap-width', `${newWidth}px`)
+          break
+        }
+        case 'bottom': {
+          // const
+          const parentInfo = this.$after_ref.get().pop().getBoundingClientRect()
+          const elementInfo = this.element.getBoundingClientRect()
+          const newHeight = parentInfo.y + parentInfo.height - elementInfo.y - (elementInfo.height / 2)
+          this.indicator.style.setProperty('--rex-indicator-wrap-height', `${newHeight}px`)
+          break
+        }
+        case 'top': {
+          const parentInfo = this.$after_ref.get().pop().getBoundingClientRect()
+          const elementInfo = this.element.getBoundingClientRect()
+          const newHeight = elementInfo.y - parentInfo.y + (elementInfo.height / 2)
+          this.indicator.style.setProperty('--rex-indicator-wrap-height', `${newHeight}px`)
+          break
+        }
+        default:
+          break
+      }
+    }
 
 	function parents(el, selector) {
 		const parents = [];
