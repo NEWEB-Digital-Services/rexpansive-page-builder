77f4a706a0c9ebe8737e4e776604bd0d1c859f06
Stefano Panipucci <stefano.panipucci@gmail.com>
2023-06-26 12:15:24 +0200

    refactor: abstract create editor function

--- public/js/live/2-CKEditor_Handler.js
+++ public/js/live/2-CKEditor_Handler.js
@@ -7,14 +7,83 @@
  * @since 2.2.0
  */
 var CKEditor_Handler = (function ($) {
-	function init() {
-		console.log(CKEDITOR)
+	let editorInstance
+
+	function createEditorInstance() {
+		editorInstance = CKEDITOR.BalloonEditor
+			.create('', {
+				plugins: [CKEDITOR.Essentials, CKEDITOR.Paragraph, CKEDITOR.Bold, CKEDITOR.Italic, CKEDITOR.Underline, CKEDITOR.Heading, CKEDITOR.FontColor, CKEDITOR.GeneralHtmlSupport, CKEDITOR.HorizontalLine, CKEDITOR.Link, CKEDITOR.Image, CKEDITOR.ImageResize, CKEDITOR.ImageStyle, CKEDITOR.ImageToolbar, CKEDITOR.Undo],
+				toolbar: [
+					'heading',
+					'bold',
+					'italic',
+					'underline',
+					'|',
+					'fontColor',
+					'horizontalLine',
+					'link',
+					'|',
+					'undo',
+					'redo'
+				],
+				heading: {
+					options: [
+						{ model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+						{ model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+						{ model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+						{ model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+						{ model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+						{ model: 'heading5', view: 'h5', title: 'Heading 5', class: 'ck-heading_heading5' },
+						{ model: 'heading6', view: 'h6', title: 'Heading 6', class: 'ck-heading_heading6' }
+					]
+				},
+				htmlSupport: {
+					allow: [
+						{
+							name: /.*/,
+							attributes: true,
+							classes: true,
+							styles: true
+						}
+					]
+				},
+				link: {
+					decorators: {
+						openInNewTab: {
+							mode: 'manual',
+							label: 'Open in new tab',
+							attributes: {
+								target: '_blank',
+								rel: 'noopener norefferer'
+							}
+						}
+					}
+				},
+				image: {
+					toolbar: [
+						'imageStyle:block',
+						'imageStyle:side',
+						'|',
+						'imageTextAlternative'
+					]
+				}
+			})
+			.then(editor => {
+				console.log('Editor was initialized', editor);
+			})
+			.catch(error => {
+				console.error(error.stack);
+			});
 	}
 
-	function load() {}
+	function init() {
+		createEditorInstance()
+	}
+
+	function load() { }
 
 	return {
 		init: init,
 		load: load
 	}
-})(jQuery);
+}) (jQuery);
